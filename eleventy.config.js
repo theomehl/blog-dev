b2682ad33cbd974bc42612f3f915ff53a1854fec
Ted Mehl <ted.mehl@gmail.com>
2023-08-19 17:36:48 -0500

    Create a blog directory with default layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 const CleanCSS = require("clean-css");
+const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
     // Return your Object options:
@@ -7,12 +8,18 @@ module.exports = function(eleventyConfig) {
         return new CleanCSS({}).minify(code).styles;
     });
 
+    eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "LLLL dd, yyyy");
+	});
+
     eleventyConfig.addPassthroughCopy("./src/fonts");
 
     return {
       dir: {
         input: "src",
-        output: "public"
+        output: "public",
+        includes: "../_includes"
       }
     }
   };
